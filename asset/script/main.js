274be930fa8c7cf189dfbc153d8d85dcf6f7b212
Bastian Gutierrez <bas.gutierrez@gmail.com>
2023-04-25 08:14:20 -0400

    Se actualiza app versión 0.002

--- asset/script/main.js
+++ asset/script/main.js
@@ -5,14 +5,59 @@ function calcular() {
     
   
     // Calcular la cantidad total de alimentos
-    var totalCarne = numAdultos*0.35+numNinos*0.15
+    var totalCarne = numAdultos*0.35+numNinos*0.15;
+    var totalVacuno=totalCarne*0.40;
+    var totalCerdo=totalCarne*0.30;
+    var totalPollo=totalCarne*0.20;
+    var totalEmbutido=totalCarne*0.10;
 
     // Crear la tabla con los resultados
-    var tabla = "<table><thead><tr><th>Carne</th><th>Cantidad</th><th>Imagen</th></tr></thead><tbody>";
-    tabla += "<tr><td>Vacuno</td><td>" + totalCarne + " Kg</td><td><img src='asset/img/carnito.jpg' alt='imagen-de-carnito'></td></tr>";
-    tabla += "</tbody></table>";
+    var tabla = '<P>Se deben comprar '+ totalCarne+' Kg de Carne</p>';
+    tabla +='</br> <P>Distribuidos de la siguiente forma:</p>';
+    tabla +='<div class="distribucion">'
+    tabla +='<div class=card-carne>'
+    tabla +='<img src="asset/img/vacuno.jpg" alt="Imagen carne">'
+    tabla +='<h4>VACUNO</h4>'
+    tabla +='<p>Puede ser lomo liso, lomo vetado, punta de ganso o cualquiera de tu preferencia</p>'
+    tabla +='<P>Total ' +totalVacuno+' Kg de Carne</p>'
+    tabla +='</div>'
+    tabla +='<div class=card-carne>'
+    tabla +='<img src="asset/img/cerdo.jpg" alt="Imagen carne">'
+    tabla +='<h4>CERDO</h4>'
+    tabla +='<p>Costillar, plateada o cualquiera de tu preferencia</p>'
+    tabla +='<P>Total ' +totalCerdo+' Kg de Carne</p>'
+    tabla +='</div>'
+    tabla +='<div class=card-carne>'
+    tabla +='<img src="asset/img/pollo.jpg" alt="Imagen carne">'
+    tabla +='<h4>POLLO</h4>'
+    tabla +='<p>Recomentación:Tutos ala</p>'
+    tabla +='<P>Total ' +totalPollo+' Kg de Carne</p>'
+    tabla +='</div>'
+    tabla +='<div class=card-carne>'
+    tabla +='<img src="asset/img/embutidos.jpg" alt="Imagen carne">'
+    tabla +='<h4>EMBUTIDOS</h4>'
+    tabla +='<p>Longanizas, chorizos, Prietas o cualquiera de tu preferencia</p>'
+    tabla +='<P>Total ' +totalEmbutido+' Kg de Carne</p>'
+    tabla +='</div>'
+    tabla +='</div>';
   
     // Mostrar la tabla en el div de resultado
     document.getElementById("resultado").innerHTML = tabla;
   }
   
+  function truncarCeroCinco(num){
+    var decimales =num % 1;
+    var resultado=0
+    if (decimales>0.5){
+      resultado=math.floor(num)+1;
+      return resultado;
+    } else{
+      resultado=math.floor(num)+0.5;
+      return resultado;
+    }
+  }
+
+  function test1(){
+    var a=truncarCeroCinco(1.1);
+    alert (a);
+  }
