4dfbab8a3df347c5e71cea1fe1e12f8584f9bdd7
Bastian Gutierrez <bas.gutierrez@gmail.com>
2023-04-25 20:46:14 -0400

    Corrección de decimales y cambios esteticos en resultado

--- asset/script/main.js
+++ asset/script/main.js
@@ -6,38 +6,34 @@ function calcular() {
   
     // Calcular la cantidad total de alimentos
     var totalCarne = numAdultos*0.35+numNinos*0.15;
-    var totalVacuno=totalCarne*0.40;
-    var totalCerdo=totalCarne*0.30;
-    var totalPollo=totalCarne*0.20;
-    var totalEmbutido=totalCarne*0.10;
+    var totalVacuno=truncarCeroCinco(totalCarne*0.40);
+    var totalCerdo=truncarCeroCinco(totalCarne*0.30);
+    var totalPollo=truncarCeroCinco(totalCarne*0.20);
+    var totalEmbutido=truncarCeroCinco(totalCarne*0.10);
+    totalCarne=totalVacuno+totalCerdo+totalEmbutido+totalPollo
 
     // Crear la tabla con los resultados
-    var tabla = '<P>Se deben comprar '+ totalCarne+' Kg de Carne</p>';
-    tabla +='</br> <P>Distribuidos de la siguiente forma:</p>';
+    var tabla = '<P>'+ totalCarne+' Kg de Carne</p>';
     tabla +='<div class="distribucion">'
     tabla +='<div class=card-carne>'
     tabla +='<img src="asset/img/vacuno.jpg" alt="Imagen carne">'
     tabla +='<h4>VACUNO</h4>'
-    tabla +='<p>Puede ser lomo liso, lomo vetado, punta de ganso o cualquiera de tu preferencia</p>'
-    tabla +='<P>Total ' +totalVacuno+' Kg de Carne</p>'
+    tabla +='<p>' +totalVacuno+' KG</p>'
     tabla +='</div>'
     tabla +='<div class=card-carne>'
     tabla +='<img src="asset/img/cerdo.jpg" alt="Imagen carne">'
     tabla +='<h4>CERDO</h4>'
-    tabla +='<p>Costillar, plateada o cualquiera de tu preferencia</p>'
-    tabla +='<P>Total ' +totalCerdo+' Kg de Carne</p>'
+    tabla +='<P>' +totalCerdo+' KG</p>'
     tabla +='</div>'
     tabla +='<div class=card-carne>'
     tabla +='<img src="asset/img/pollo.jpg" alt="Imagen carne">'
     tabla +='<h4>POLLO</h4>'
-    tabla +='<p>Recomentación:Tutos ala</p>'
-    tabla +='<P>Total ' +totalPollo+' Kg de Carne</p>'
+    tabla +='<p>' +totalPollo+' KG</p>'
     tabla +='</div>'
     tabla +='<div class=card-carne>'
     tabla +='<img src="asset/img/embutidos.jpg" alt="Imagen carne">'
     tabla +='<h4>EMBUTIDOS</h4>'
-    tabla +='<p>Longanizas, chorizos, Prietas o cualquiera de tu preferencia</p>'
-    tabla +='<P>Total ' +totalEmbutido+' Kg de Carne</p>'
+    tabla +='<p>' +totalEmbutido+' KG</p>'
     tabla +='</div>'
     tabla +='</div>';
   
@@ -47,17 +43,12 @@ function calcular() {
   
   function truncarCeroCinco(num){
     var decimales =num % 1;
-    var resultado=0
+    var resultado=0;
     if (decimales>0.5){
-      resultado=math.floor(num)+1;
+      resultado=num-num%1+1;
       return resultado;
     } else{
-      resultado=math.floor(num)+0.5;
+      resultado=num-num%1+0.5;
       return resultado;
     }
-  }
-
-  function test1(){
-    var a=truncarCeroCinco(1.1);
-    alert (a);
   }
